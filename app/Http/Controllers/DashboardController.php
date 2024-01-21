abb94f06cacb349bc284707f6916782618e07061
Sabotazh <sabotazh001@gmail.com>
2024-01-21 15:22:48 +0200

    add the admin user to the database seeds

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function __invoke(): void
+    public function __invoke(): RedirectResponse
     {
-        'admin' === Auth::user()->role
+        return 'admin' === Auth::user()->role
             ? redirect()->route('admin.dashboard')
             : redirect()->route('employee.dashboard');
     }
