bb6b4e988d33d7c3bbbd398fc4851219c9c47e5d
Sabotazh <sabotazh001@gmail.com>
2024-01-21 15:50:42 +0200

    add CRUD functionality for Companies and Employees

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -34,5 +36,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('companies', CompanyController::class);
+
+Route::resource('employees', EmployeeController::class);
 
 require __DIR__.'/auth.php';
