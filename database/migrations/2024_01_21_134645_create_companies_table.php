dfd42476cd95f075b1ccd2977bef69b0eaf979a9
Sabotazh <sabotazh001@gmail.com>
2024-01-21 16:10:27 +0200

    update companies' migration and model

--- database/migrations/2024_01_21_134645_create_companies_table.php
+++ database/migrations/2024_01_21_134645_create_companies_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('logo');
+            $table->string('website');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('companies');
